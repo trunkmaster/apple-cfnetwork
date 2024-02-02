12a94830fc38f5c11207ff8373031f9e5fc1b94d
Sergii Stoian <stoyan255@gmail.com>
2024-02-02 17:21:26 +0000

    gethostbyname: get and print IP adresses for resolved hostname.

--- Tests/gethostbyname/gethostbyname.c
+++ Tests/gethostbyname/gethostbyname.c
@@ -1,18 +1,41 @@
 #include <CoreFoundation/CFLogUtilities.h>
 #include <CFNetwork/CFNetwork.h>
 
-void ClientCallback(CFHostRef theHost, CFHostInfoType typeInfo, const CFStreamError* error, CFStringRef name)
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+
+void printAddressFromData(CFDataRef data)
 {
-  CFArrayRef addrs;
+  struct sockaddr *ai_addr = (struct sockaddr *)CFDataGetBytePtr(data);
+  struct sockaddr_in *addr = (struct sockaddr_in *)ai_addr;
+
+  CFLog(kCFLogLevelInfo, CFSTR("->-> %s"), inet_ntoa((struct in_addr)addr->sin_addr));
+}
+
+void ClientCallback(CFHostRef theHost, CFHostInfoType typeInfo, const CFStreamError* error, void *info)
+{
+  CFHostRef host = (CFHostRef)info;
+  CFArrayRef addrs, names;
   Boolean hasBeenResolved;
 
-  CFLog(kCFLogLevelInfo, CFSTR("Client callback called"));  
-  if (!error->error) {
-    CFLog(kCFLogLevelInfo, CFSTR("Addresses resolution finished without error. Good."));
-    addrs = CFHostGetAddressing(theHost, &hasBeenResolved);
-    CFShow(addrs);
-  }
   CFHostSetClient(theHost, NULL, NULL);
+
+  CFLog(kCFLogLevelInfo, CFSTR("-> Client callback called"));
+  if (!error->error) {
+    names = CFHostGetNames(host, &hasBeenResolved);
+    addrs = CFHostGetAddressing(theHost, &hasBeenResolved);
+    CFLog(kCFLogLevelInfo, CFSTR("-> Address %@ without error"), hasBeenResolved ? CFSTR("resolved") : CFSTR("not resolved"));
+
+    CFLog(kCFLogLevelInfo, CFSTR("-> Addresses for host name: %@"), CFArrayGetValueAtIndex(names, 0));
+    for (int i = 0; i < CFArrayGetCount(addrs); i++) {
+      printAddressFromData(CFArrayGetValueAtIndex(addrs, i));
+    }
+    CFRelease(addrs);
+  }
+
+  CFRunLoopStop(CFRunLoopGetCurrent());
 }
 
 
@@ -21,11 +44,10 @@ int main(int argc, char **argv)
   CFHostRef host = CFHostCreateWithName(kCFAllocatorDefault, CFSTR("localhost"));
   CFStreamError error;
   CFRunLoopRef runloop = CFRunLoopGetCurrent();
-  CFHostClientContext ctxt = {0, NULL, NULL, NULL, NULL};
+  CFHostClientContext ctxt = {0, (void *)host, NULL, NULL, NULL};
 
   CFLog(kCFLogLevelInfo, CFSTR("Setup resolution process..."));
   CFHostSetClient(host, (CFHostClientCallBack)ClientCallback, &ctxt);
-  // CFHostSetClient(host, NULL, NULL);
   CFHostScheduleWithRunLoop(host, runloop, kCFRunLoopDefaultMode);
 
   CFLog(kCFLogLevelInfo, CFSTR("Starting addresses resolution..."));
@@ -35,5 +57,4 @@ int main(int argc, char **argv)
   }
 
   CFRunLoopRun();
-  // CFRunLoopRunInMode(kCFRunLoopCommonModes, 10, TRUE);
 }
